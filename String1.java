4f80df0b1902cdbee16ff042bd8715704e04f689
Prashant Singh <134587691+prashantsingh05@users.noreply.github.com>
2023-08-16 20:55:21 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,14 @@
+public class String1 {
+    public static void main(String[] args) {
+        // String 
+        // Concatenate
+        String name1 = "Prashant";
+        String name2 = "Nishant";
+        String name3 = "Nidhish";
+        String name4 = "Shilu";
+        String name5 = "Neha";
+        String name6 = name1 + " and " +  name2 + " and " + name3 + " and " + name4 + " and " + name5;
+        System.out.println(name6);
+
+    }
+}
